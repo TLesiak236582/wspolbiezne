55834bf425e2569b23c4d6571c8d717881269991
TLesiak236582 <102290031+TLesiak236582@users.noreply.github.com>
2022-03-24 23:32:21 +0100

    testy

---
+++ UnitTest1.cs
@@ -0,0 +1,14 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestProject2
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMultiplication2()
+        {
+            Assert.AreNotEqual(4,Wspó³bie¿ne.Program.multiplication(2, 3));
+        }
+    }
+}

fd187548a4fcf2f49e3f99d98affa32012841834
TLesiak236582 <102290031+TLesiak236582@users.noreply.github.com>
2022-03-24 23:34:40 +0100

    Add files via upload

---
+++ mnozenie/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Współbieżne
+{
+    public class Program
+    {
+
+        public static int multiplication(int x, int y)
+        {
+            return x * y;
+        }
+
+        static void Main(string[] args)
+        {
+           Console.WriteLine("Program wykona mnożenie dwóch liczb wprowadzonych przez użytkownika")
+            Console.WriteLine("Podaj pierwsza liczbe:");
+            int x = int.Parse(Console.ReadLine());
+            Console.WriteLine("Podaj druga liczbe:");
+            int y = int.Parse(Console.ReadLine());
+
+
+            Console.WriteLine("Wynik mnozenia: " + multiplication(x, y));
+        }
+    }
+}
